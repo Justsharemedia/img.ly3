ba748eceb18d5f4a207a0ed9eeb57b518d231060
Justsharemedia <justin@justsharemedia.com>
2024-05-24 13:50:06 -0600

    reverting most of the postMessage changes

--- src/components/case/CaseComponent.jsx
+++ src/components/case/CaseComponent.jsx
@@ -9,34 +9,55 @@ const CaseComponent = () => {
   let page;
 
   useEffect(() => {
-    const handleMessage = (event) => {
-      if (event.origin !== "https://justin-16657.bubbleapps.io") {
-        console.error("Received message from unauthorized origin:", event.origin);
-        return;
-      }
-      
-      const { video_path, brand_logo, customer_id, video_id, video_location } = event.data;
-      console.log("Received data via postMessage:", event.data);
-      
-      if (cesdkContainer.current && !cesdk) {
-        initializeSDK(video_path, brand_logo, video_id, video_location, customer_id);
-      }
+    const getVideoPathFromUrl = () => {
+      const search = window.location.search;
+      const params = new URLSearchParams(search);
+      const videoPath = params.get('video_path'); 
+      console.log('Video Path used:', videoPath);
+      return videoPath; 
     };
 
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-      if (cesdk) {
-        cesdk.dispose();
-        console.log("CESDK instance disposed");
-      }
+    const getVideoIDFromURL = () => {
+      const search = window.location.search;
+      const params = new URLSearchParams(search);
+      const videoID = params.get('video_id');
+      console.log('VideoID used:', videoID);
+      return videoID;
     };
-  }, []);
 
-  const initializeSDK = (videoPath, brandLogoUrl, videoId, videoLocation, customerId) => {
-    console.log("Initializing SDK with data:", { videoPath, brandLogoUrl, videoId, videoLocation, customerId });
-    CreativeEditorSDK.create(cesdkContainer.current, {
+    const getVideoLocationFromURL = () => {
+      const search = window.location.search;
+      const params = new URLSearchParams(search);
+      const videoLocation = params.get('video_location');
+      console.log('Video Location used:', videoLocation);
+      return videoLocation;
+    };
+
+    const getBrandLogoFromUrl = () => {
+      const search = window.location.search;
+      const params = new URLSearchParams(search);
+      const brandLogoUrl = params.get('brand_logo');
+      console.log('Brand Logo used:', brandLogoUrl)
+      return brandLogoUrl;
+    };   
+  
+    const videoPath = getVideoPathFromUrl();
+    console.log('Final Video Path used:', videoPath);
+
+    const params = new URLSearchParams(window.location.search);
+    const customer_id = params.get('customer_id');
+    console.log('Got the customer_ID', customer_id);
+
+    const videoID = getVideoIDFromURL();
+    console.log('VideoID used:', videoID);
+  
+    const videoLocation = getVideoLocationFromURL();
+    console.log('Video Location used:', videoLocation);
+
+    const brandLogoUrl = getBrandLogoFromUrl();
+    console.log('Final Brand Logo Path used:', brandLogoUrl);
+
+    const config = {
       role: 'Adopter',
       theme: 'dark',
       baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.25.0/assets',
@@ -45,19 +66,22 @@ const CaseComponent = () => {
       },
       callbacks: {
         onUnsupportedBrowser: () => {
-          window.alert('Your current browser is not supported.\nPlease use one of the following:\n\n- Mozilla Firefox 86 or newer\n- Apple Safari 14.1 or newer\n- Microsoft Edge 88 or newer\n- Google Chrome 88 or newer');
+          window.alert(
+            'Your current browser is not supported.\nPlease use one of the following:\n\n- Mozilla Firefox 86 or newer\n- Apple Safari 14.1 or newer\n- Microsoft Edge 88 or newer\n- Google Chrome 88 or newer'
+          );
         },
         onExport: async (scene) => {
           console.info('Export callback!');
+      
           const data = {
-            timestamp: new Date().toISOString(),
-            customer_id: customerId,
-            video_id: videoId,
+            timestamp: new Date().toISOString(), 
+            customer_id: customer_id, 
+            video_id: videoID,
             video_location: videoLocation,
             callback_type: 'export',
             callback_message: 'User triggered export'
           };
-
+      
           fetch('https://justin-16657.bubbleapps.io/version-test/api/1.1/wf/editor_update_user', {
             method: 'POST',
             headers: {
@@ -66,10 +90,15 @@ const CaseComponent = () => {
             body: JSON.stringify(data)
           })
           .then(response => response.json())
-          .then(data => console.log('Success:', data));
+          .then(data => console.log('Success:', data))
+          exportVideo();
         },
-        onBack: () => window.alert('Back callback!'),
-        onClose: () => window.alert('Close callback!'),
+        onBack: () => {
+          window.alert('Back callback!');
+        },
+        onClose: () => {
+          window.alert('Close callback!');
+        },
         onSave: (scene) => {
           window.alert('Save callback!');
           console.info(scene);
@@ -79,6 +108,7 @@ const CaseComponent = () => {
           console.info(scene);
         },
       },
+
       ui: {
         elements: {
           panels: {
@@ -96,56 +126,111 @@ const CaseComponent = () => {
         }
       },
       license: process.env.NEXT_PUBLIC_LICENSE
-    }).then(async (instance) => {
-      cesdk = instance;
-      console.log("CESDK instance created:", cesdk);
-      // Additional SDK setup can be performed here
-    }).catch(error => {
-      console.error("Error initializing CESDK:", error);
-    });
-  };
+    };
+  
+    if (cesdkContainer.current) {
+      CreativeEditorSDK.create(cesdkContainer.current, config).then(
+        async (instance) => {
+          cesdk = instance;
+          const engine = cesdk.engine;
+          const layout = 'Horizontal';
+    
+          let scene = engine.scene.createVideo();
+          page = engine.block.create('page');
+          await engine.block.appendChild(scene, page);
+          await engine.block.setWidth(page, 1080);
+          await engine.block.setHeight(page, 1080);
 
-  // Setup editor with video and brand logo
-  const setupEditor = (cesdk, videoPath, brandLogoUrl) => {
-    const engine = cesdk.engine;
-    let scene = engine.scene.createVideo();
-    page = engine.block.create('page');
-    engine.block.appendChild(scene, page);
+          cesdk.engine.editor.setSettingBool('page/title/show', false);
+          await cesdk.addDefaultAssetSources();
+          await cesdk.addDemoAssetSources({ sceneMode: 'Video' });
+  
+          engine.block.setDuration(page, 60);
+           
+          const solidColor = engine.block.createFill('color');
+          const rgbaBlack = { r: 0, g: 0, b: 0, a: 1 };
+          engine.block.setColor(solidColor, 'fill/color/value', rgbaBlack);
+          engine.block.setFill(page, solidColor);
+  
+          if (videoPath) {
+            const videoBlock = engine.block.create('graphic');
+            engine.block.setShape(videoBlock, engine.block.createShape('rect'));
+            const videoFill = engine.block.createFill('video');
+            engine.block.setString(videoFill, 'fill/video/fileURI', videoPath);
+            engine.block.setFill(videoBlock, videoFill);
+  
+            engine.block.setDuration(videoBlock, 60);
+            engine.block.setPositionX(videoBlock, 0);
+            engine.block.setPositionXMode(videoBlock, 'Absolute');
+            engine.block.setPositionY(videoBlock, 236.25);
+            engine.block.setPositionYMode(videoBlock, 'Absolute');
+            engine.block.setWidth(videoBlock, 1080);
+            engine.block.setWidthMode(videoBlock, 'Absolute');
+            engine.block.setHeight(videoBlock, 607.5);
+            engine.block.setHeightMode(videoBlock, 'Absolute');
+  
+            await engine.block.appendChild(page, videoBlock);
+          }
+    
+          if (brandLogoUrl) {
+            const logoBlock = engine.block.create('graphic');
+            engine.block.setShape(logoBlock, engine.block.createShape('rect'));
+            const logoFill = engine.block.createFill('image');
+            engine.block.setString(logoFill, 'fill/image/fileURI', brandLogoUrl);
+            engine.block.setFill(logoBlock, logoFill);
+      
+            engine.block.setPositionX(logoBlock, 10); 
+            engine.block.setPositionY(logoBlock, 10); 
+            engine.block.setWidth(logoBlock, 100); 
+            engine.block.setHeight(logoBlock, 100); 
+      
+            await engine.block.appendChild(page, logoBlock);
+          }
 
-    engine.block.setWidth(page, 1080);
-    engine.block.setHeight(page, 1080);
-    cesdk.engine.editor.setSettingBool('page/title/show', false);
-    cesdk.addDefaultAssetSources();
-    cesdk.addDemoAssetSources({ sceneMode: 'Video' });
-
-    // Video setup
-    const videoBlock = engine.block.create('graphic');
-    engine.block.setShape(videoBlock, engine.block.createShape('rect'));
-    const videoFill = engine.block.createFill('video');
-    engine.block.setString(videoFill, 'fill/video/fileURI', videoPath);
-    engine.block.setFill(videoBlock, videoFill);
-    engine.block.setDuration(videoBlock, 60);
-    engine.block.setPositionX(videoBlock, 0);
-    engine.block.setPositionY(videoBlock, 236.25);
-    engine.block.setWidth(videoBlock, 1080);
-    engine.block.setHeight(videoBlock, 607.5);
-    engine.block.appendChild(page, videoBlock);
-
-    // Brand logo setup
-    if (brandLogoUrl) {
-      const logoBlock = engine.block.create('graphic');
-      engine.block.setShape(logoBlock, engine.block.createShape('rect'));
-      const logoFill = engine.block.createFill('image');
-      engine.block.setString(logoFill, 'fill/image/fileURI', brandLogoUrl);
-      engine.block.setFill(logoBlock, logoFill);
-      engine.block.setPositionX(logoBlock, 10); 
-      engine.block.setPositionY(logoBlock, 10); 
-      engine.block.setWidth(logoBlock, 100); 
-      engine.block.setHeight(logoBlock, 100); 
-      engine.block.appendChild(page, logoBlock);
+          engine.scene.zoomToBlock(page);
+        }
+      );
     }
-
-    engine.scene.zoomToBlock(page);
+  
+    return () => {
+      if (cesdk) {
+        cesdk.dispose();
+      }
+    };
+  
+  }, [cesdkContainer]);
+  
+  const exportVideo = async () => {
+    if (!cesdk || !page) {
+      console.error('CESDK or page is not properly initialized.');
+      return;
+    }
+    const mimeType = 'video/mp4';
+    const progressCallback = (renderedFrames, encodedFrames, totalFrames) => {
+      console.log(
+        'Rendered',
+        renderedFrames,
+        'frames and encoded',
+        encodedFrames,
+        'frames out of',
+        totalFrames
+      );
+    };
+    const videoOptions = {
+      targetWidth: 1080,
+      targetHeight: 1080,
+    };
+    try {
+      const videoBlob = await cesdk.engine.block.exportVideo(page, mimeType, progressCallback, videoOptions);
+      
+      // Download the exported video
+      const anchor = document.createElement('a');
+      anchor.href = URL.createObjectURL(videoBlob);
+      anchor.download = 'exported-video.mp4';
+      anchor.click();
+    } catch (error) {
+      console.error('Error exporting video:', error);
+    }
   };
 
   return (
@@ -159,12 +244,21 @@ const CaseComponent = () => {
 
 const cesdkStyle = {
   position: 'absolute',
-  top: 0, right: 0, bottom: 0, left: 0
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0
 };
 
 const cesdkWrapperStyle = {
-  position: 'relative', minHeight: '610', overflow: 'hidden', flexGrow: 1, display: 'flex',
-  boxShadow: '0px 0px 2px rgba(22, 22, 23, 0.25), 0px 4px 6px -2px rgba(22, 22, 23, 0.12), 0px 2px 2.5px -2px rgba(22, 22, 23, 0.12), 0px 1px 1.75px -2px rgba(22, 22, 23, 0.12)'
+  position: 'relative',
+  minHeight: '610',
+  overflow: 'hidden',
+  flexGrow: 1,
+  display: 'flex',
+  boxShadow:
+    '0px 0px 2px rgba(22, 22, 23, 0.25), 0px 4px 6px -2px rgba(22, 22, 23, 0.12), 0px 2px 2.5px -2px rgba(22, 22, 23, 0.12), 0px 1px 1.75px -2px rgba(22, 22, 23, 0.12)'
 };
+
 
 export default CaseComponent;
